fa431b5824bbcdaabc69c96fc8af9629acbf6e47
TiagoSnts <tsc.3602006@gmail.com>
2026-08-25 22:14:47 -0300

    primeiros exercicios

---
+++ ex1.py
@@ -0,0 +1,7 @@
+nome = str(input("Digite seu nome: "))
+idade = int(input("Digite sua idade: "))
+sexo = str(input("Digite seu sexo: "))
+endereco = str(input("Digite seu endereco: "))
+telefone = str(input("Digite seu telefone: "))
+
+print("\nInfos: \nnome: {}\nidade: {}\nsexo: {}\nendereço: {}\ntelefone: {}".format(nome, idade, sexo, endereco, telefone))
---
+++ ex2.py
@@ -0,0 +1,7 @@
+nome = str(input("Digite seu nome: "))
+bim1 = int(input("Digite sua nota do bimestre 1: "))
+bim2 = int(input("Digite sua nota do bimestre 2: "))
+bim3 = int(input("Digite sua nota do bimestre 3: "))
+bim4 = int(input("Digite sua nota do bimestre 4: "))
+
+print(f"\nNota Bimestre 1: {bim1}\nNota Bimestre 2: {bim2}\nNota Bimestre 3: {bim3}\nNota Bimestre 4: {bim4}")
